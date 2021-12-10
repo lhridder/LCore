127e5ee933414d7a69b66bdf5fbe0cf9a0368a76
lhridder <lhridder@gmail.com>
2021-12-10 23:22:05 +0100

    Add more possible Statistics

--- src/main/java/nl/lucasridder/lcore/Managers/Statistics.java
+++ src/main/java/nl/lucasridder/lcore/Managers/Statistics.java
@@ -2,13 +2,31 @@ package nl.lucasridder.lcore.Managers;
 
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public class Statistics {
 
+    // Log kill
     public static void registerKill(Player killer) {
+        UUID uuid = killer.getUniqueId();
         //TODO yeet into db
     }
 
+    // Log death
     public static void registerDeath(Player victim) {
+        UUID uuid = victim.getUniqueId();
+        //TODO yeet into db
+    }
+
+    // Log nexus destroy
+    public static void registerDestroy(Player player) {
+        UUID uuid = player.getUniqueId();
+        //TODO yeet into db
+    }
+
+    // Log win
+    public static void registerWin(Player player) {
+        UUID uuid = player.getUniqueId();
         //TODO yeet into db
     }
 
