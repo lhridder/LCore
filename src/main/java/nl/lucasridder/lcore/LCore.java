2731b41f70ddaf59324d15dce4c88052afa3e7cc
lhridder <lhridder@gmail.com>
2021-12-17 20:21:51 +0100

    start working on arena manager

--- src/main/java/nl/lucasridder/lcore/LCore.java
+++ src/main/java/nl/lucasridder/lcore/LCore.java
@@ -1,11 +1,17 @@
 package nl.lucasridder.lcore;
 
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import com.onarandombox.MultiverseCore.utils.WorldManager;
 import nl.lucasridder.lcore.Commands.JoinCommand;
 import nl.lucasridder.lcore.Commands.MapCommand;
 import nl.lucasridder.lcore.Commands.PlaytimeCommand;
 import nl.lucasridder.lcore.Commands.StatsCommand;
 import nl.lucasridder.lcore.Listeners.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 public class LCore extends JavaPlugin {
 
@@ -13,10 +19,21 @@ public class LCore extends JavaPlugin {
     public LCore() {
         plugin = this;
     }
+    public static MVWorldManager wm;
 
     @Override
     public void onEnable() {
         //config
+        if (!getDataFolder().exists()) {
+            //create config folder
+            try {
+                getDataFolder().createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            plugin.getConfig().options().copyDefaults(true);
+        }
 
         //register listeners
         getServer().getPluginManager().registerEvents(new JoinEvent(), this);
@@ -37,6 +54,13 @@ public class LCore extends JavaPlugin {
         this.getCommand("map").setExecutor(new MapCommand());
         this.getCommand("stats").setExecutor(new StatsCommand());
         this.getCommand("playtime").setExecutor(new PlaytimeCommand());
+
+        MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+        wm = core.getMVWorldManager();
+    }
+
+    @Override
+    public void onDisable() {
 
     }
 
--- src/main/java/nl/lucasridder/lcore/Managers/Arenas.java
+++ src/main/java/nl/lucasridder/lcore/Managers/Arenas.java
@@ -1,9 +1,59 @@
 package nl.lucasridder.lcore.Managers;
 
+import nl.lucasridder.lcore.LCore;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Arenas {
+    static String currentMap = "";
+    static String currentWorld = "";
+    Location lobby = null;
+    static ArrayList<String> maps = new ArrayList<>();
+    public static FileConfiguration config = LCore.plugin.getConfig();
 
-    public void regenArena() {
+    public void getArenas() {
+        // fetch lobby coords
+        lobby.setX(config.getDouble("lobby.x"));
+        lobby.setY(config.getDouble("lobby.y"));
+        lobby.setZ(config.getDouble("lobby.z"));
+        lobby.setWorld(Bukkit.getWorld(config.getString("lobby.world")));
+        // fetch maps
+        List<String> maps = config.getStringList("maplist");
+        for (String map : maps) {
+            config.get("maps." + map);
+        }
+    }
 
+    public void newArena() {
+        // Choose new map
+        Random rand = new Random();
+        ArrayList<String> newmaps = maps;
+        newmaps.remove(currentMap);
+        int map = rand.nextInt(newmaps.size());
+        String newmap = newmaps.get(map);
+        // Load map
+        LCore.wm.cloneWorld(newmap, newmap + "_active");
+        Location newmapspawn = null;
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.teleport(newmapspawn);
+        }
+        currentMap = newmap;
+        // Delete old map
+        LCore.wm.deleteWorld(currentWorld);
+        currentMap = newmap;
+        currentWorld = newmap + "_active";
+    }
+
+    public void closeArenas() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.teleport(lobby);
+        }
+        LCore.wm.deleteWorld(currentWorld);
     }
 
 }
