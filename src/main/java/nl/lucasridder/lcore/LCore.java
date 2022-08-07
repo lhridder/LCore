decddcfd44929c3e28a11845f26a9f417010f7d0
Marijn2341 <marijn1232@hotmail.com>
2022-08-07 19:38:28 +0200

    Keep your onEnable clean

--- src/main/java/nl/lucasridder/lcore/LCore.java
+++ src/main/java/nl/lucasridder/lcore/LCore.java
@@ -8,6 +8,7 @@ import nl.lucasridder.lcore.Commands.PlaytimeCommand;
 import nl.lucasridder.lcore.Commands.StatsCommand;
 import nl.lucasridder.lcore.Listeners.*;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LCore extends JavaPlugin {
@@ -25,29 +26,9 @@ public class LCore extends JavaPlugin {
 
         try {
             //register listeners
-            getServer().getPluginManager().registerEvents(new JoinEvent(), this);
-            getServer().getPluginManager().registerEvents(new BreakEvent(), this);
-            getServer().getPluginManager().registerEvents(new PlaceEvent(), this);
-            getServer().getPluginManager().registerEvents(new RespawnEvent(), this);
-            getServer().getPluginManager().registerEvents(new DamageByEntityEvent(), this);
-            getServer().getPluginManager().registerEvents(new ChatEvent(), this);
-            getServer().getPluginManager().registerEvents(new DeathEvent(), this);
-            getServer().getPluginManager().registerEvents(new JoinEvent(), this);
-            getServer().getPluginManager().registerEvents(new WeatherListener(), this);
-            getServer().getPluginManager().registerEvents(new FoodListener(), this);
-            getServer().getPluginManager().registerEvents(new InteractEvent(), this);
-            getServer().getPluginManager().registerEvents(new MoveEvent(), this);
-            getServer().getPluginManager().registerEvents(new PreCommandEvent(), this);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
+            registerEvents();
             //register commands
-            plugin.getCommand("join").setExecutor(new JoinCommand());
-            plugin.getCommand("map").setExecutor(new MapCommand());
-            plugin.getCommand("stats").setExecutor(new StatsCommand());
-            plugin.getCommand("playtime").setExecutor(new PlaytimeCommand());
+            registerCommands();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -63,6 +44,32 @@ public class LCore extends JavaPlugin {
     @Override
     public void onDisable() {
         //TODO unload
+    }
+
+    public void registerEvents() {
+
+        PluginManager m = getServer().getPluginManager();
+
+        m.registerEvents(new JoinEvent(), this);
+        m.registerEvents(new BreakEvent(), this);
+        m.registerEvents(new PlaceEvent(), this);
+        m.registerEvents(new RespawnEvent(), this);
+        m.registerEvents(new DamageByEntityEvent(), this);
+        m.registerEvents(new ChatEvent(), this);
+        m.registerEvents(new DeathEvent(), this);
+        m.registerEvents(new JoinEvent(), this);
+        m.registerEvents(new WeatherListener(), this);
+        m.registerEvents(new FoodListener(), this);
+        m.registerEvents(new InteractEvent(), this);
+        m.registerEvents(new MoveEvent(), this);
+        m.registerEvents(new PreCommandEvent(), this);
+    }
+
+    public void registerCommands() {
+        plugin.getCommand("join").setExecutor(new JoinCommand());
+        plugin.getCommand("map").setExecutor(new MapCommand());
+        plugin.getCommand("stats").setExecutor(new StatsCommand());
+        plugin.getCommand("playtime").setExecutor(new PlaytimeCommand());
     }
 
 }
